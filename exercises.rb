3403511e084a91c80f0e77640efbb2dd47045714
Scott <scotthuston1982@gmail.com>
2017-12-13 13:25:32 -0500

    exercise1 finished

---
+++ exercises.rb
@@ -0,0 +1,37 @@
+colours = ["blue", "red", "orange", "cyan", "green"]
+ages = [35, 33, 34, 24, 21]
+coin = ["tails", "tails","heads","tails","tails"]
+artists = ["john","dave","sally"]
+colour = [:blue, :red, :orange, :cyan, :green]
+
+definitoins = {
+  house: "A single dwelling structure people live in",
+  laptop: "A small portable computer",
+  chair: "A piece of furniture for one person to sit on"
+}
+
+movies = {
+  terminator2: "2001",
+  shawshank_redemption: "1996",
+  casino: "1995"
+}
+
+cities = {
+  new_york: "14 million",
+  toronto: "3.2 million",
+  london: "5.6 million"
+}
+
+people = {
+  scott: 35,
+  jimmy: 33,
+  Liz: 34,
+  shawn: 24,
+  rob: 21
+}
+#Exercise 1
+puts coin
+puts colours[0]
+ages.push 0
+puts ages.sort
+puts movies.values[0]
